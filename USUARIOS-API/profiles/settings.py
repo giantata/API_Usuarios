e3caccaf8bae80fa3382bff0d6cbdc7182853575
giantata <giankyguti@gmail.com>
2021-07-25 16:02:58 -0300

    Update settings.py

--- USUARIOS-API/profiles/settings.py
+++ USUARIOS-API/profiles/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ysjni)400o!&+06f6(xc-s=^&kx=x87m1f_smprae8%+(ildd('
+SECRET_KEY = 'django-insecure-ysjni)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
